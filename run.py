cc9c933c8005a1b6d78b687ac5537dcfc62b3735
erianafranky <arianadottie@gmail.com>
2019-10-23 11:09:34 +0300

    Modify the main file

--- run.py
+++ run.py
@@ -4,35 +4,55 @@ import random, copy
 from random import shuffle
 from random import sample
 import os, time, datetime
+import io
+import questions as qm
 from questions import*
 
 app = Flask(__name__)
 app.secret_key = os.urandom(24)
 
+def shuffle(q):
+    selected_keys = []
+    i = 0
+    while i < len(q):
+        current_selection = random.choice(list(q))
+        if current_selection not in selected_keys:
+            selected_keys.append(current_selection)
+            i = i + 1
+        return selected_keys
 
 
-@app.route('/social', methods=['GET', 'POST'])
-def social():
-    with open('TemplateQuiz.csv', 'rU') as csvfile:
+@app.route('/phishing', methods=['GET', 'POST'])
+def phishing():
+    with open('TemplateQuiz .csv', 'rU') as csvfile:
         reader = csv.reader(csvfile)
-        for row in reader:
-            print (row)            
-            quiz = list(reader)  
-    q = random.choice(quiz)
+        quiz = list(reader)
+   
+        question =quiz[current_question_index][0]
+        options = quiz[current_question_index][1:-1]
+        correct_answer = quiz[current_question_index][-1]
+        current_question = phishings()
+        current_answer = request.form.get('i')
+        for i in question:
+            answered = request.form.get(i)
+            if i[0] == answered:
+                question["correct"].append(str(int(session[current_question_index])))
+            #else:
+             #   questions["wrong"].append(str(int(session[current_question_index])))
+            
+        #session[current_question_index] = session[current_question_index]+1
+
+   
     
     if "current_question" not in session:
         session["current_question"] = "1"
 
-    elif session["current_question"] not in quiz:
-        questions["wrong"] = list(set(questions["wrong"]))
-        questions["correct"] = list(set(questions["correct"]))
     currentN=int(session["current_question"])
     currentQ = random.choice(quiz)
-
+    question_shuffled= shuffle(quiz)
     app.nquestions = len(quiz)
-   
-
-    return render_template('socials.html', num=currentN, ntot=app.nquestions, question=currentQ)
+    return render_template('socials.html', q=question_shuffled, o=question, num=currentN, ntot=app.nquestions)
 
 if __name__ == '__main__':
     app.run(debug=True)
+
